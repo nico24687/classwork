273c15c797e0091c88b24c1aca65ddc39ddb9868
nico24687 <theonlynico@hotmail.com>
2017-10-04 10:52:53 -0600

    finished itteration three of bad connection

--- bad_connection.rb
+++ bad_connection.rb
@@ -34,58 +34,126 @@
 
 #Iteration 2
 
-def greeting()
-    puts "HELLO THIS IS A GROCERY STORE"
-end
+# def greeting()
+#     puts "HELLO THIS IS A GROCERY STORE"
+# end
 
-def no_response()
-    puts "HELLO?"
+# def no_response()
+#     puts "HELLO?"
+# end 
+
+# def require_input()
+#     input = gets.chomp
+# end 
+
+# def downcase_response()
+#     puts "I AM HAVING A HARD TIME HEARING YOU"
+# end
+
+# def upcase_response()
+#     puts "NO THIS IS NOT A PET STORE"
+# end
+
+# def intial_goodbye()
+#     puts "ANYTHING ELSE I CAN HELP YOU WITH"
+# end
+
+# def final_goodbye()
+#     puts "THANK YOU FOR CALLING"
+# end
+# counter = 0 
+# ready_to_quit = false
+
+
+# greeting
+# input = gets.chomp
+
+# until ready_to_quit
+#     if input.empty?
+#         no_response
+#         input = gets.chomp
+#     elsif input == input.downcase
+#         downcase_response
+#         input = gets.chomp
+#     elsif input == "GOODBYE!" && counter > 0
+#         ready_to_quit = true
+#     elsif input == "GOODBYE!"
+#         counter += 1 
+#         intial_goodbye
+#         input = gets.chomp
+#     elsif input == input.upcase
+#         upcase_response
+#         input = gets.chomp
+#     end
+
+# end
+
+# final_goodbye
+
+
+#Iteration 3 
+
+class StoreTeller
+
+    attr_accessor 
+
+    def greeting()
+        puts "HELLO THIS IS A GROCERY STORE"
+    end
+
+    def no_response()
+        puts "HELLO?"
+    end 
+    
+    def require_input()
+        input = gets.chomp
+    end 
+    
+    def downcase_response()
+        puts "I AM HAVING A HARD TIME HEARING YOU"
+    end
+    
+    def upcase_response()
+        puts "NO THIS IS NOT A PET STORE"
+    end
+    
+    def intial_goodbye()
+        puts "ANYTHING ELSE I CAN HELP YOU WITH"
+    end
+    
+    def final_goodbye()
+        puts "THANK YOU FOR CALLING"
+    end
+
 end 
 
-def require_input()
-    input = gets.chomp
-end 
+cashier1 = StoreTeller.new()
 
-def downcase_response()
-    puts "I AM HAVING A HARD TIME HEARING YOU"
-end
-
-def upcase_response()
-    puts "NO THIS IS NOT A PET STORE"
-end
-
-def intial_goodbye()
-    puts "ANYTHING ELSE I CAN HELP YOU WITH"
-end
-
-def final_goodbye()
-    puts "THANK YOU FOR CALLING"
-end
+ready_to_quit = false 
 counter = 0 
-ready_to_quit = false
+cashier1.greeting
+input = gets.chomp 
 
-
-greeting
-input = gets.chomp
 
 until ready_to_quit
+
     if input.empty?
-        no_response
+        cashier1.no_response()
         input = gets.chomp
     elsif input == input.downcase
-        downcase_response
+        cashier1.downcase_response
         input = gets.chomp
     elsif input == "GOODBYE!" && counter > 0
-        ready_to_quit = true
+        ready_to_quit = true 
     elsif input == "GOODBYE!"
         counter += 1 
-        intial_goodbye
+        cashier1.intial_goodbye
         input = gets.chomp
     elsif input == input.upcase
-        upcase_response
-        input = gets.chomp
+        cashier1.upcase_response
+        input = gets.chomp 
     end
 
 end
 
-final_goodbye
+cashier1.final_goodbye
